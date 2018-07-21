676b5ca0dd102bf3f674a36f0d244f4485c39f5e
oluwaslim <f.alofe@proteuxtechnologies.net>
2018-07-21 23:23:46 +0100

    Import Farmers

---
+++ app/Rules/ExcelFile.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class ExcelFile implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $ext = request()->file('farmers') ?
+            request()->file('farmers')->getClientOriginalExtension() :
+            false;
+
+        return in_array($ext,['xls','xlsx']);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Only xls,xlsx file is valid.';
+    }
+}
